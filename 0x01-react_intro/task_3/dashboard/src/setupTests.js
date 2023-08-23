ce461c5893d77ba8afac0741ad29f90ddbd2d8e3
OzimiWhoke <ozimi11zz@gmail.com>
2023-08-23 16:18:15 +0000

    basic test

---
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -0,0 +1,7 @@
+// learn more: https://github.com/testing-library/jest-dom
+// import '@testing-library/jest-dom';
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-17-updated';
+
+Enzyme.configure({ adapter: new Adapter() });
